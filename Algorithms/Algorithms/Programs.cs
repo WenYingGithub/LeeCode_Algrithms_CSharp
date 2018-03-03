10791997ea3847a3f8732b12ddeaea9fb68744e2
Wen Ying <ywen03@syr.edu>
2018-03-03 18:41:45 -0500

    simple console project

---
+++ Algorithms/Algorithms/Programs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Programs
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input the Problem Number to begin: \r\n\r\n Problem Number : ");
+
+            int problemNumber = Convert.ToInt32( Console.ReadLine());
+
+            string outPut = "";
+
+            switch (problemNumber)
+            {
+                case 349:
+                    Solutions hi = new Solutions();
+                    outPut = hi.hrrrr();
+                    break;
+
+                default:
+                    outPut = "The End";
+                    break;
+
+            }
+            Console.WriteLine("\r\n\r\nResult: " + outPut);
+            Console.ReadKey();
+        }
+
+    }
+
+    public class Solutions
+    {
+        public string hrrrr()
+        {
+            return "hhh";
+        }
+    }
+}
--- Algorithms/Algorithms/Properties/AssemblyInfo.cs
+++ Algorithms/Algorithms/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("405b34b5-0df0-488e-8b57-166c6291bc0b")]
+[assembly: Guid("99f5a615-8e20-4f4e-b873-a8cffb4cc126")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
